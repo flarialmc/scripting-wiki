2fb7363e4293a5deed4dec8fca36231a34ec9686
Raspberry <84133368+EpiclyRaspberry@users.noreply.github.com>
2024-11-23 02:40:34 +0800

    amongus-ception

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,24 @@
 import "../styles/Sidebar.css"
 import Collapsible from "react-collapsible"
+import { BsChevronDown } from "react-icons/bs"
 
 function Sidebar () {
     return (
         <>
-            <Collapsible trigger="Documentation">
-            <p>ashdga</p>
-            </Collapsible>
+        <div className="sidebar">
+            <Collapsible trigger={["Documentation", <BsChevronDown size={32}/>]}>
+            <p>among us</p>
+                <Collapsible trigger={["Documentation", <BsChevronDown size={32}/>]}>
+                    <p>among us</p>
+                    <Collapsible trigger={["Documentation", <BsChevronDown size={32}/>]}>
+                        <p>among us</p>
+                        <Collapsible trigger={["Documentation", <BsChevronDown size={32}/>]}>
+                            <p>among us</p>
+                        </Collapsible> 
+                    </Collapsible> 
+                </Collapsible> 
+            </Collapsible> 
+        </div>
         </>
     )
 }
